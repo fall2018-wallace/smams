3313883533e7d4973702a7306007df9854630bee
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 12:05:00 -0500

    Saving AirlineName.R

--- AirlineName.R
+++ AirlineName.R
@@ -1,1 +1,3 @@
 
+res_Airline <- SurveyData %>% group_by(Airline_Name) %>% summarise(Freq=n(), percLessThan3= sum(Satisfaction<3, na.rm = TRUE))
+res_Airline$perc <- (res_Airline$percLessThan3/res_Airline$Freq)*100

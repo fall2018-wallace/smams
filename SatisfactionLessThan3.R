93013c4509c2e00aed5bde694c4730fc182b61d4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 12:07:13 -0500

    Saving SatisfactionLessThan3.R

--- SatisfactionLessThan3.R
+++ SatisfactionLessThan3.R
@@ -1,3 +1,11 @@
 
 res <- SurveyData %>% group_by(Age) %>% summarise(Freq=n(), percLessThan3= sum(Satisfaction<3, na.rm = TRUE))
 res$perc <- (res$percLessThan3/res$Freq)*100
+
+res_Airline <- SurveyData %>% group_by(Airline_Name) %>% summarise(Freq=n(), percLessThan3= sum(Satisfaction<3, na.rm = TRUE))
+res_Airline$perc <- (res_Airline$percLessThan3/res_Airline$Freq)*100
+
+
+ggplot(res_Airline, aes(x=Airline_Name ,y = Freq, fill=perc))+geom_col()+theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+ggplot(res, aes(x=Age ,y = Freq, fill=perc))+geom_col()+theme(axis.text.x = element_text(angle = 90, hjust = 1))
